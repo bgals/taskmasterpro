d851ff709bed013e24d702c4dc5d6134050897f3
bgallen <bgallen24@gmail.com>
2022-06-01 17:00:17 -0700

    draggable and droppable

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,4 +182,67 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+//*Make list items draggable and sortable
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  //make dropped items stay in columns when page is refreshed
+  update: function() {
+      var tempArr = []
 
+      $(this).children().each(function() {
+        var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      tempArr.push({
+          text: text,
+          date: date
+      });  
+      });
+      var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+      console.log(tempArr);
+  }
+});
+//make draggable items droppable in trash
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+       //this specifically  
+       ui.draggable.remove();
+      console.log("drop");
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
